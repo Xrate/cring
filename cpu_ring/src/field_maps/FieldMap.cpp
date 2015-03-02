5a311fa93ec5176b6557d940c3fb1331402c7ad5
Stanislav Sandurskyi <xrate1ua@gmail.com>
2015-03-02 14:59:50 +0200

    work on FieldMap

--- cpu_ring/src/field_maps/FieldMap.cpp
+++ cpu_ring/src/field_maps/FieldMap.cpp
@@ -1,6 +1,18 @@
 #include "FieldMap.h"
 
-const shared_ptr<const FieldMap> FieldMap::getFieldMap(const string& fileName)
+map<string, shared_ptr<const FieldMap>> FieldMap::fieldMaps;
+
+shared_ptr<const FieldMap> FieldMap::getFieldMap(const string& fileName)
+{
+	auto it = fieldMaps.find(fileName);
+	if (it != fieldMaps.end())
+		return it->second;
+
+	fieldMaps[fileName] = shared_ptr<const FieldMap>(new FieldMap(fileName));
+	return fieldMaps.at(fileName);
+}
+
+FieldMap::FieldMap(const string& fileName)
 {
 	throw exception("Map logic not implemented");
 }
--- cpu_ring/src/field_maps/FieldMap.h
+++ cpu_ring/src/field_maps/FieldMap.h
@@ -6,11 +6,11 @@
 class FieldMap
 {
 public:
-	static const shared_ptr<const FieldMap> getFieldMap(const string& fileName);
+	static shared_ptr<const FieldMap> getFieldMap(const string& fileName);
 
 	double getFieldValue(double x, double y, double z);
 private:
-	static map<string, const shared_ptr<const FieldMap>> fieldMaps;
+	static map<string, shared_ptr<const FieldMap>> fieldMaps;
 
 	FieldMap(const string& fileName);
 	double *rawMap;
